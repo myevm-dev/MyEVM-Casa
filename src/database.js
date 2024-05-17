92fbcc7ba2bfab918a0ba5078b8dbf23fb277192
L7 <161780994+l7cubed@users.noreply.github.com>
2024-05-17 02:58:56 +0000

    base

--- src/database.js
+++ src/database.js
@@ -2,7 +2,14 @@ export const websiteData = {
   categories: {
     "Dapps": [
       {
+        name: "PTBook",
+        url: "https://ptbook.xyz/",
+        image: "/images/ptbook.png",
+        code: "https://github.com/l7cubed",
+      },
+      {
         name: "Migration App",
+        
         url: "https://migrate.cabana.fi/",
         image: "/images/migration.png",
         code: "https://github.com/GenerationSoftware/pooltogether-client-monorepo",
@@ -11,12 +18,6 @@ export const websiteData = {
         name: "PTList",
         url: "https://ptlist.xyz/",
         image: "/images/ptlist.png",
-      },
-      {
-        name: "PTBook",
-        url: "https://ptbook.xyz/",
-        image: "/images/ptbook.png",
-        code: "https://github.com/splash-xyz",
       },
       {
         name: "Cabana Lists",
@@ -146,9 +147,34 @@ export const websiteData = {
         image: "/images/beefy.png",
       },
       {
-        name: "Sonne Finance",
-        url: "https://sonne.finance/",
-        image: "/images/sonne.png",
+        name: "Coinbase",
+        url: "https://coinbase.com/",
+        image: "/images/coinbase.png",
+      },
+      {
+        name: "Lido",
+        url: "https://lido.fi/",
+        image: "/images/lido.png",
+      },
+      {
+        name: "Velodrome Finance",
+        url: "https://velodrome.finance/",
+        image: "/images/velodrome.png",
+      },
+      {
+        name: "Aerodrome Finance",
+        url: "https://aerodrome.finance/",
+        image: "/images/aerodrome.png",
+      },
+      {
+        name: "Aloe Capital",
+        url: "https://Aloe.capital/",
+        image: "/images/aloe.png",
+      },
+      {
+        name: "Pool Together V5 Staking",
+        url: "https://pooltogether.com/",
+        image: "/images/ptstaking.png",
       },
     ],
     "prize-pools": [
@@ -163,6 +189,12 @@ export const websiteData = {
         url: "https://splash.wtf",
         image: "/images/opethprize.png",
         code: "https://optimistic.etherscan.io/address/0xF35fE10ffd0a9672d0095c435fd8767A7fe29B55#code",
+      },
+      {
+        name: "ETH Prize Pool",
+        url: "https://splash.wtf",
+        image: "/images/baseprizepool.png",
+        code: "https://basescan.org/address/0x45b2010d8a4f08b53c9fa7544c51dfd9733732cb#code",
       },
     ],
     "prize-hooks": [
@@ -200,7 +232,7 @@ export const websiteData = {
       {
         name: "Pfers",
         url: "https://opensea.io/collection/pooly-pfers",
-        image: "/images/comingsoon.png",
+        image: "/images/pfers.png",
         code: "https://etherscan.io/address/0xbcc664b1e6848caba2eb2f3de6e21f81b9276dd8"
       },
     ],
@@ -213,14 +245,49 @@ export const websiteData = {
     ],
     "Opchain-Contracts": [
       {
-        name: "Rewards Builder",
-        url: "https://ptbook.xyz/",
-        image: "/images/opetherscan.png",
+        name: "TWAB Controller",
+        url: "https://optimistic.etherscan.io/address/0xCB0672dE558Ad8F122C0E081f0D35480aB3be167",
+        image: "/images/optwab.png",
       },
       {
-        name: "Claimer",
-        url: "https://ptbook.xyz/",
-        image: "/images/opetherscan.png",
+        name: "TWAB Rewards",
+        url: "https://optimistic.etherscan.io/address/0x90D383dEA4dcE52D3e5D3C93dE75eF36da3Ea9Ea",
+        image: "/images/optwabrewards.png",
+      },
+      {
+        name: "TpdaLiquidationPairFactory",
+        url: "https://optimistic.etherscan.io/address/0x80F86691632d9863E6bCaa472e5c34574F77c7D1",
+        image: "/images/opliqfactory.png",
+      },
+      {
+        name: "TpdaLiquidationRouter",
+        url: "https://optimistic.etherscan.io/address/0x7766b5E6839a1a218Fc861b0810C504490876136#code",
+        image: "/images/opliqrouter.png",
+      },
+      {
+        name: "Pool Prize Vault",
+        url: "https://optimistic.etherscan.io/address/0xa52e38a9147f5eA9E0c5547376c21c9E3F3e5e1f",
+        image: "/images/poolprizetokenvaultop.png",
+      },
+      {
+        name: "Draw Manager",
+        url: "https://optimistic.etherscan.io/address/0x7eED7444dE862c4F79c5820ff867FA3A82641857",
+        image: "/images/opdrawmgr.png",
+      },
+      {
+        name: "ClaimerFactory",
+        url: "https://optimistic.etherscan.io/address/0x498C92bEF017A91018ecCAE29b3b3C531e3f4794#code",
+        image: "/images/opclaimerfactory.png",
+      },
+      {
+        name: "Staking Vault",
+        url: "https://optimistic.etherscan.io/address/0x2C30004EC09e731DfFCD5783569fb2E09C473732",
+        image: "/images/opstaking.png",
+      },
+      {
+        name: "Claimer Example",
+        url: "https://optimistic.etherscan.io/address/0x0b5a1dc536D5A67C66D00B337E6b189385BD8438",
+        image: "/images/opclaimer.png",
       },
       {
         name: "Draw Fund",
@@ -228,24 +295,19 @@ export const websiteData = {
         image: "/images/opetherscan.png",
       },
       {
-        name: "Prize Pool",
-        url: "https://ptbook.xyz/",
-        image: "/images/opetherscan.png",
+        name: "Weth Prize Pool",
+        url: "https://optimistic.etherscan.io/address/0xF35fE10ffd0a9672d0095c435fd8767A7fe29B55",
+        image: "/images/opprzpool.png",
       },
       {
-        name: "Random Number",
-        url: "https://ptbook.xyz/",
-        image: "/images/opetherscan.png",
+        name: "RNGWitnet",
+        url: "https://optimistic.etherscan.io/address/0x3d2Ef6C091f7CB69f06Ec3117F36A28BC596aa7B#code",
+        image: "/images/oprng.png",
       },
       {
-        name: "Yield Liquidators",
-        url: "https://ptbook.xyz/",
-        image: "/images/opetherscan.png",
-      },
-      {
-        name: "Vault Factory",
-        url: "https://ptbook.xyz/",
-        image: "/images/opetherscan.png",
+        name: "PrizeVaultFactory",
+        url: "https://optimistic.etherscan.io/address/0x0C379e9b71ba7079084aDa0D1c1Aeb85d24dFD39#code",
+        image: "/images/opprizevaultfactory.png",
       },
       {
         name: "Protocol Overview",
@@ -255,39 +317,69 @@ export const websiteData = {
     ],
     "Basechain-Contracts": [
       {
-        name: "Rewards Builder",
-        url: "https://ptbook.xyz/",
-        image: "/images/basescan.png",
+        name: "TWAB Controller",
+        url: "https://basescan.org/address/0x7e63601f7e28c758feccf8cdf02f6598694f44c6",
+        image: "/images/basetwab.png",
       },
       {
-        name: "Claimer",
-        url: "https://ptbook.xyz/",
-        image: "/images/basescan.png",
+        name: "TWAB Rewards",
+        url: "https://basescan.org/address/0x86f0923d20810441efc593eb0f2825c6bff2dc09",
+        image: "/images/basetwabrewards.png",
       },
       {
-        name: "Draw Fund",
-        url: "https://ptbook.xyz/",
-        image: "/images/basescan.png",
+        name: "TpdaLiqPairFactory",
+        url: "https://basescan.org/address/0x8557a9a33b573dc4403708c5a8746a52648374ea",
+        image: "/images/baseliqfactory.png",
       },
       {
-        name: "Prize Pool",
-        url: "https://ptbook.xyz/",
-        image: "/images/basescan.png",
+        name: "TpdaLiquidationRouter",
+        url: "https://basescan.org/address/0xa9c937a0d1d22ad79099aea10efa62a270dfc22c",
+        image: "/images/baseliqrouter.png",
       },
       {
-        name: "Random Number",
-        url: "https://ptbook.xyz/",
-        image: "/images/basescan.png",
+        name: "Pool Prize Vault",
+        url: "https://optimistic.etherscan.io/address/0xa52e38a9147f5eA9E0c5547376c21c9E3F3e5e1f",
+        image: "/images/poolprizetokenvaultbase.png",
       },
       {
-        name: "Yield Liquidators",
-        url: "https://ptbook.xyz/",
-        image: "/images/basescan.png",
+        name: "Draw Manager",
+        url: "https://basescan.org/address/0x8a2782bedc79982ebfa3b68b315a2ee40daf6ab0",
+        image: "/images/basedrawmgr.png",
       },
       {
-        name: "Vault Factory",
-        url: "https://ptbook.xyz/",
-        image: "/images/basescan.png",
+        name: "Claimer Factory",
+        url: "https://basescan.org/address/0xd58a04fc8d34ce6b3633bf81ee7d5d25c71401e3",
+        image: "/images/baseclaimerfactory.png",
+      },
+      {
+        name: "Staking Vault",
+        url: "https://basescan.org/address/0x854f3d91e53d0fc9dba9822bc74f574c7380436d",
+        image: "/images/basestaking.png",
+      },
+      {
+        name: "Claimer Example",
+        url: "https://basescan.org/address/0x5ffeee76d1e2d2d1d18ba0bc77d8d047b85e1e87",
+        image: "/images/baseclaimer.png",
+      },
+      {
+        name: "Vault Booster Factory",
+        url: "https://basescan.org/address/0x02d37f2654b4161801c8d7369b43a8762a31530e",
+        image: "/images/vaultboost.png",
+      },
+      {
+        name: "Weth Prize Pool",
+        url: "https://basescan.org/address/0x45b2010d8a4f08b53c9fa7544c51dfd9733732cb",
+        image: "/images/baseprzpool.png",
+      },
+      {
+        name: "RNGWitnet",
+        url: "https://basescan.org/address/0x74ebf391831c0757b5a4335f2f3abbb1499d18f0",
+        image: "/images/baserng.png",
+      },
+      {
+        name: "PrizeVaultFactory",
+        url: "https://basescan.org/address/0xe32f6344875494ca3643198d87524519dc396ddf",
+        image: "/images/baseprizefactory.png",
       },
       {
         name: "Protocol Overview",
@@ -342,7 +434,7 @@ export const websiteData = {
       {
         name: "Pooly Merch Store",
         url: "https://pooly.me/",
-        image: "/images/comingsoon.png",
+        image: "/images/merch.png",
       },
     ],
   },
